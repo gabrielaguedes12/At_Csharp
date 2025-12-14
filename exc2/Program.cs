d8e7f61acca4352fa0fb7d85fb2801ed430372e8
gabrielaguedes12 <gabr1.profissional@gmail.com>
2025-12-14 00:26:43 -0300

    subindo acertos

--- exc2/Program.cs
+++ exc2/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace exc2
+﻿using System;
+namespace exc2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Digite seu nome completo: ");
+            string nome = Console.ReadLine();
+
+            char[] caracteres = nome.ToCharArray();
+
+            for (int i = 0; i< caracteres.Length; i++)
+            {
+                char c = caracteres[i];
+
+                if (c >= 'A' && c<= 'Z')
+                {
+                    caracteres[i] = (char)(((c - 'A' + 2)% 26)+'A');
+                }
+                else if (c >= 'a' && c<= 'z')
+                {
+                    caracteres[i] = (char)(((c - 'a' + 2) % 26) + 'a');
+                }
+            }
+            string resultado = new string(caracteres);
+            Console.WriteLine("Nome cifrado: " + resultado);
         }
     }
-}
